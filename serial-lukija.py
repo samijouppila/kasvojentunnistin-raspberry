574d23433e8cb2698cf18905c770a7a35f43f537
Eero <t7soee00@students.oamk.fi>
2019-11-28 09:44:38 +0200

    Modified code

--- serial-lukija.py
+++ serial-lukija.py
@@ -4,7 +4,11 @@ import time
 s = serial.Serial('COM4')
 t_end = time.time() + 60 * 2
 
+luku = input("Anna 0 tai 1  ")
 
-while time.time() < t_end:
-     res = s.read()
-     print(res)
+if luku == 1:
+
+     while time.time() < t_end:
+               res = s.read()
+               print(res)
+luku = input("Anna luku uudestaan  ")
