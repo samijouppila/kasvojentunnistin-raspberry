c393446250a0bac9bf0cfca83a58c0e450c84018
Eero <t7soee00@students.oamk.fi>
2019-11-28 12:51:44 +0200

    Updated code

--- hc04_lukija.py
+++ hc04_lukija.py
@@ -1,22 +1,30 @@
 import serial
-import time
 import io 
+
+
+
 
 s = serial.Serial('COM4')
 
 
-res = s.read()
 
-value1 = res
+def Lukija():
+    while True:
+        #lukee sarjaporttia ja antaa valuen        s = serial.Serial('COM4')
+        res = s.read()
+        value1 = res
+        #muuttaan serialista luetun stringin kokonaisluvuksi
+        muutettuValue = int(value1)
+                
 
-#muuttaan serialista luetun stringin kokonaisluvuksi
-muutettuValue = int(value1)
+        if muutettuValue == 1:
+            
+            return True
+        
 
 
-if muutettuValue == 1:
-    print("Aktivoitu")
 
-elif muutettuValue == 0:
-    print("Ei aktiivinen")
-else:
-    print("Vaara luku")
+
+
+
+Lukija()
